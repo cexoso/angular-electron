6f3da8c704d2e7e37064d4c92fcac94864c30e36
熊杰 <xiongj01@mysoft.com.cn>
2017-12-12 09:58:42 +0800

    no console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,14 @@ export class AppComponent {
 
     translate.setDefaultLang('en');
 
-    if (electronService.isElectron()) {
-      console.log('Mode electron');
-      // Check if electron is correctly injected (see externals in webpack.config.js)
-      console.log('c', electronService.ipcRenderer);
-      // Check if nodeJs childProcess is correctly injected (see externals in webpack.config.js)
-      console.log('c', electronService.childProcess);
-    } else {
-      console.log('Mode web');
-    }
+    // if (electronService.isElectron()) {
+    //   console.log('Mode electron');
+    //   // Check if electron is correctly injected (see externals in webpack.config.js)
+    //   console.log('c', electronService.ipcRenderer);
+    //   // Check if nodeJs childProcess is correctly injected (see externals in webpack.config.js)
+    //   console.log('c', electronService.childProcess);
+    // } else {
+    //   console.log('Mode web');
+    // }
   }
 }
